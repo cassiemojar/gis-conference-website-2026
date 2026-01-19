8c224599d41d38f0d247486c2a994e8e80ce7dff
llivvv <livialin2014@gmail.com>
2026-01-18 23:53:36 -0500

    start agenda

---
+++ src/data/timelineData.js
@@ -0,0 +1,17 @@
+export const timelineData = [
+  {
+    time: "10:00 - 10:30 AM",
+    name: "Introduction",
+    location: "CHBE 101",
+  },
+  {
+    time: "10:30 - 12:00 PM",
+    name: "Workshops",
+    location: "ALSSC",
+  },
+  {
+    time: "12:00 - 1:00 PM",
+    name: "LUNCH",
+    location: "everywhere",
+  },
+];
--- src/sections/Agenda.jsx
+++ src/sections/Agenda.jsx
@@ -1,4 +1,46 @@
-function Agenda() {
-  return <div>Agenda</div>;
+import "../styles/Agenda.css";
+import { timelineData } from "../data/timelineData";
+
+const AgendaItem = ({ time, name, location }) => {
+  return (
+    <div className='agenda-item'>
+      <div className='event-time'>
+        <p className='p1'>{time}</p>
+      </div>
+      <div className='separator'>
+        {/* vertical line: lhs */}
+        <div className='lhs'>
+          <div className='point'></div>
+        </div>
+        <div className='rhs'></div>
+      </div>
+      <div className='name-loc'>
+        <div className='event-name-container'>
+          <p className='p1 event-name'>{name}</p>
+        </div>
+        <div className='at'>@</div>
+        <div className='loc'>
+          <p className='p1'>{location}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function Agenda() {
+  return (
+    <div className='agenda'>
+      <div className='agenda-section'>
+        <h2>AGENDA</h2>
+        {timelineData.map((data, index) => (
+          <AgendaItem
+            key={`timeline_${index}`}
+            time={data.time}
+            name={data.name}
+            location={data.location}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-export default Agenda;
