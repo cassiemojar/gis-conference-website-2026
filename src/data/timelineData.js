9d1c6fe36984243fa5b3e5527f2f88947c6d82e7
llivvv <livialin2014@gmail.com>
2026-01-31 17:06:57 -0500

    Desktop view

--- src/data/timelineData.js
+++ src/data/timelineData.js
@@ -14,4 +14,44 @@ export const timelineData = [
     name: "LUNCH",
     location: "everywhere",
   },
+  {
+    time: "10:00 - 10:30 AM",
+    name: "Introduction",
+    location: "CHBE 101",
+  },
+  {
+    time: "10:30 - 12:00 PM",
+    name: "Workshops",
+    location: "ALSSC",
+  },
+  {
+    time: "12:00 - 1:00 PM",
+    name: "LUNCH",
+    location: "everywhere",
+  },
+  {
+    time: "10:00 - 10:30 AM",
+    name: "Introduction",
+    location: "CHBE 101",
+  },
+  {
+    time: "10:30 - 12:00 PM",
+    name: "Workshops",
+    location: "ALSSC",
+  },
+  {
+    time: "12:00 - 1:00 PM",
+    name: "LUNCH",
+    location: "everywhere",
+  },
+  {
+    time: "10:00 - 10:30 AM",
+    name: "Introduction",
+    location: "CHBE 101",
+  },
+  {
+    time: "10:30 - 12:00 PM",
+    name: "Workshops",
+    location: "ALSSC",
+  },
 ];
--- src/sections/Agenda.jsx
+++ src/sections/Agenda.jsx
@@ -1,43 +1,52 @@
 import "../styles/Agenda.css";
 import { timelineData } from "../data/timelineData";
+import { Typography, Box } from "@mui/material";
 
-const AgendaItem = ({ time, name, location }) => {
+const AgendaItem = ({ time, name, location, isLast }) => {
   return (
-    <div className='agenda-item'>
+    <>
       <div className='event-time'>
         <p className='p1'>{time}</p>
       </div>
       <div className='separator'>
-        {/* vertical line: lhs */}
-        <div className='lhs'>
-          <div className='point'></div>
-        </div>
-        <div className='rhs'></div>
+        <div className='separator-circle'></div>
+        {!isLast && <div className='separator-vert-line'></div>}
+        <div className='separator-horiz-line'></div>
       </div>
-      <div className='name-loc'>
-        <div className='event-name-container'>
-          <p className='p1 event-name'>{name}</p>
-        </div>
-        <div className='at'>@</div>
-        <div className='loc'>
-          <p className='p1'>{location}</p>
-        </div>
+      <div className='name-loc-card'>
+        <p className='p1 event-name'>{name}</p>
+        <p className='p1 loc'>@ {location}</p>
       </div>
-    </div>
+    </>
   );
 };
 
 export default function Agenda() {
   return (
     <div className='agenda'>
-      <div className='agenda-section'>
-        <h2>AGENDA</h2>
+      {/* <h2>AGENDA</h2> */}
+      <Typography
+        variant='h4'
+        sx={{
+          textAlign: "center",
+          color: "#E8C3D1",
+          fontFamily: "Josefin Sans",
+          fontWeight: "bold",
+          marginBottom: "40px",
+          fontSize: { xs: "2rem", sm: "2.5rem", md: "3rem" }, // Responsive font size
+          animation: "slideIn 1s ease-out", // Apply slide-in animation
+        }}
+      >
+        AGENDA
+      </Typography>
+      <div className='agenda-grid'>
         {timelineData.map((data, index) => (
           <AgendaItem
             key={`timeline_${index}`}
             time={data.time}
             name={data.name}
             location={data.location}
+            isLast={index === timelineData.length - 1}
           />
         ))}
       </div>
