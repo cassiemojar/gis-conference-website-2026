82766b488ec3c51bffd858e199a210bdcf18fe24
Cassandra <cassiemojar09@gmail.com>
2026-02-13 18:36:36 -0800

    start about us

---
+++ src/data/AboutUsData.jsx
@@ -0,0 +1,12 @@
+import img1 from '../images/navbar-logo.png';
+import img2 from '../images/logo-yt.png';
+
+
+
+export const AboutUsData = {
+  content: [
+    "At InnovateHER, our mission is to cultivate a dynamic space for high school and college students in the Greater Vancouver Area to explore and engage with Science, Technology, Engineering, Arts, and Maths (STEAM) alongside principles of Accessibility. Our full-day event is dedicated to bridging the gender gap in STEM industries by providing immersive workshops, insightful panels, and interactive experiences that embrace all dimensions of STEAM, including Art and Accessibility."
+  ],
+  /* Placeholder images */
+  images: [img1, img2]
+};
