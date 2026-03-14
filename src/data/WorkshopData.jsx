6b557678bb5d79d129ac4e2d4df15354743b2cae
Cassandra <cassiemojar09@gmail.com>
2026-03-13 21:46:06 -0700

    added workshop data

--- src/data/WorkshopData.jsx
+++ src/data/WorkshopData.jsx
@@ -3,22 +3,31 @@ export const workshopData = {
       workshops: [
         {
           time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "FL",
-          name: "First Last (Pro/nouns)",
-          role: "Research Lead",
+          topic: "TBD!!!",
+          initials: "AP",
+          name: "Alysa Parker (Pro/nouns)",
+          role: "Environmental Program Lead – FortisBC",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer facilisis, sem ac posuere luctus, lectus velit bibendum nunc, sit amet cursus metus neque vitae nulla.",
+            "Alysha Parker has a diverse background spanning wildlife monitoring, major infrastructure projects, and resource development. She has held roles at BC Hydro's Site C, Kiewit on the Trans Mountain Pipeline, and Teck Resources, and now drives environmental stewardship across complex energy initiatives at FortisBC.",
+      },
+        {
+          time: "2:00PM - 3:00PM",
+          topic: "Introduction to GWAS: Genome-wide association studies (Application of Bioinformatics)",
+          initials: "KH",
+          name: "Kaede Hirabayashi (She/Her)",
+          role: "PhD student in Tom Booker lab – UBC Forestry and Environmental Stewardship",
+          description:
+            "Kaede studies genome architecture and speciation patterns in conifers, with a passion for plant biodiversity through an evolutionary lens. Zhiqin specializes in plant evolution, bioinformatics, local adaptation, and positive selection, focusing on adaptive evolution in wild sunflowers.",
         },
         {
-          time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "SN",
-          name: "Sample Name (Pro/nouns)",
-          role: "Lab Coordinator",
+          time: "2:00PM - 3:00PM",
+          topic: "Introduction to GWAS: Genome-wide association studies (Application of Bioinformatics)",
+          initials: "ZL",
+          name: "Zhiqin Long (She/Her)",
+          role: "PhD candidate – Botany Department of UBC",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat, nibh et pharetra volutpat, sem purus mattis velit, vitae lacinia erat lorem ac est.",
-        },
+            "Zhiqin Long is a PhD candidate in the Department of Botany at UBC, specializing in plant evolution, bioinformatics, local adaptation, and positive selection. Her research focuses on how positive selection drives adaptive evolution in wild sunflowers.",
+        }
       ],
     },
   
@@ -26,21 +35,21 @@ export const workshopData = {
       workshops: [
         {
           time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "PG",
-          name: "Panel Guest (Pro/nouns)",
-          role: "Software Engineer",
+          topic: "Designing Tech for Everyone: How Software Teams Build Products for Real People",
+          initials: "MP",
+          name: "Maddy Paulson (She/Her)",
+          role: "Software Developer in the Graduate Technology Leadership Program – TELUS",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper, neque non gravida volutpat, mauris est laoreet elit, vitae placerat tellus nulla at ipsum.",
+            "Maddy Paulson is a software developer currently working as an iOS developer on the SmartHome+ app at TELUS as part of the Graduate Technology Leadership Program. She graduated in May 2025 from the University of British Columbia with a degree in Computer Engineering. As part of UBC’s Co-op program, Maddy gained two years of hands-on experience in DevOps, software development, software testing, and automation. She is passionate about building reliable, user-focused software and solving complex problems through thoughtful engineering.",
         },
         {
-          time: "11:00AM - 12:00PM",
+          time: "2:00PM - 3:00PM",
           topic: "Workshop Topic",
-          initials: "FS",
-          name: "Future Speaker (Pro/nouns)",
-          role: "UX Researcher",
+          initials: "SP",
+          name: "Shefali Pearson (She/Her)",
+          role: "Lead Game Product Manager – Electronic Arts",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non turpis vitae sapien posuere aliquet. Mauris varius sem at turpis luctus, nec posuere lectus feugiat.",
+            "Shefali Pearson is a Lead Game Product Manager at Electronic Arts, currently working on skate. With over a decade of experience in product management and a background in software engineering, she brings a strong blend of technical expertise and strategic thinking to game development. Before entering the gaming industry, she founded her own fashion label. She is passionate about mentoring women in STEM and helping students explore careers in technology, product management, and gaming.",
         },
       ],
     },
@@ -49,22 +58,31 @@ export const workshopData = {
       workshops: [
         {
           time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "GE",
-          name: "Guest Expert (Pro/nouns)",
-          role: "Mechanical Engineer",
+          topic: "Where Engineering Meets Sustainability and Social Equity",
+          initials: "CGM",
+          name: "Cecilia Garcia Marra (She/Her)",
+          role: "Process Design Team Lead – McCue Engineering Contractors",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse potenti. Curabitur feugiat orci at erat tincidunt, a efficitur sem ullamcorper.",
+            "Cecilia Garcia Marra, P.Eng., is a senior engineering leader and quality systems specialist known for bringing clarity, structure, and rigor to complex water and wastewater treatment challenges. With more than a decade of experience in process design and multidisciplinary project delivery, she blends deep technical expertise with a steady, analytical leadership style that strengthens teams and elevates engineering practice. Her career includes leading major treatment system designs, developing internal engineering standards, and advancing organizational knowledge through training and national conference presentations. A committed mentor and educator, she has supported P.Eng. applicants, coordinated tutoring programs, and contributed extensively to advancing women in STEM. Outside of STEM, she enjoys dancing, baking, and making arts & crafts with her two little boys.",
         },
         {
           time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "AL",
-          name: "Alex Lee (Pro/nouns)",
-          role: "Design Engineer",
+          topic: "Where Engineering Meets Sustainability and Social Equity",
+          initials: "MRV",
+          name: "Marialejandra Reyes Valerio (She/Her)",
+          role: "Director – Expansion Works",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare lectus ut justo pretium, quis suscipit est ultrices. Integer congue faucibus massa.",
+            "Marialejandra Reyes Valerio is a facilitator and coach originally from Venezuela who transitioned from a 10+ year career in engineering to social consulting. Over the past five years, she has been actively engaged in Canada’s nonprofit sector, focusing on women and immigrant initiatives. Through her facilitation firm, Expansion Works, she provides expertise in community engagement and learning solutions to strengthen leadership, organizational effectiveness, and capacity in equity, diversity, and inclusion.",
         },
+        {
+          time: "2:00PM - 3:00PM",
+          topic: "Discover Semiconductors and the Semiconductor Industry",
+          initials: "GX",
+          name: "Guangrui Xia (She/Her)",
+          role: "Professor – Department of Materials Engineering, UBC",
+          description:
+            "Prof. Guangrui Xia, 夏光睿, was born in Handan, China. She is currently a professor with UBC Materials Engineering (http://mtrl.ubc.ca/faculty/guangrui-xia/). Before joining UBC in 2008, she worked at IBM Semiconductor Research and Development Center, where she led the research of process modeling and simulations for 32 nm node CMOS technologies. Prof. Xia has studied SiGe for more than two decades. Her group has a strong existence in the area of SiGe and SiGe:C processing and modeling. Process models from her work have been widely used in practice and implemented in the state-of-the-art semiconductor process simulation tools. In recent years, her research group has been one of the leading research groups in the research of Ge-based III-V lasers. Her research interests also extend to GaN power transistors and through-silicon-via technology for 3D integration.",
+        }
       ],
     },
   
@@ -72,21 +90,21 @@ export const workshopData = {
       workshops: [
         {
           time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "JM",
-          name: "Jordan May (Pro/nouns)",
-          role: "Creative Director",
+          topic: "Ideas That Redefine What’s Possible",
+          initials: "AF",
+          name: "Angie Feret (She/Her)",
+          role: "Motion Graphics Designer Specializing in Future User Interface Design and Masters Of Digital Media Student and CDM",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec tellus in eros euismod fermentum. Morbi a nibh non justo cursus porttitor.",
+            "Angie is a Motion Graphics Designer specializing in Future User Interface (FUI) design and currently a Master of Digital Media student at the Centre for Digital Media. With experience across film, television, education and digital media, she has worked on projects including Tron: Ares, Sonic the Hedgehog, and Batwoman, creating on-screen interfaces and graphics that blend storytelling with emerging technology. Currently, Angie works as a motion designer for film as well as with real-world clients through her master’s program, designing digital products that bridge science fiction and practical technology. Angie focuses on making complex ideas approachable, creative and fun.",
         },
         {
-          time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "RT",
-          name: "Riley Taylor (Pro/nouns)",
-          role: "Visual Artist",
+          time: "2:00PM - 3:00PM",
+          topic: "Building a Game Character Through an Accessible 3D Pipeline: Understanding the Full Process and Where to Begin",
+          initials: "KD",
+          name: "Kira Dou (She/Her)",
+          role: "Grad Student – Centre for Digital Media",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum, velit non commodo gravida, eros lectus feugiat purus, non pretium lectus lorem at velit.",
+            "Kira is a 3D artist with a background in animation and experience at a game studio. She works across the 3D pipeline, from modeling and texturing to animation and lighting. She is passionate about making games and creating characters and worlds with strong visual identity and atmosphere.",
         },
       ],
     },
@@ -95,22 +113,31 @@ export const workshopData = {
       workshops: [
         {
           time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "DK",
-          name: "Devon Kim (Pro/nouns)",
-          role: "Math Educator",
+          topic: "Getting Acquainted Over Math; a Matriarch of Mathematics Welcomes You",
+          initials: "PG",
+          name: "Priscilla (Cindy) Greenwood (She/Her)",
+          role: "Emeritus Professor Specializing in Stochastic Modelling",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada feugiat mauris, non tristique elit fermentum id. Curabitur iaculis euismod augue.",
+            "Priscilla (Cindy) Greenwood is an emeritus professor and long-standing probabilist at the University of British Columbia. Her research focuses on stochastic modelling, with current work in neuroscience and population dynamics. Over the course of her career, she has contributed extensively to the field of probability and mathematical modelling.",
         },
         {
-          time: "11:00AM - 12:00PM",
-          topic: "Workshop Topic",
-          initials: "MS",
-          name: "Morgan Shah (Pro/nouns)",
-          role: "Data Modeler",
+          time: "2:00PM - 3:00PM",
+          topic: "Careers in Public Health - From Data to Decisions: How Models Can Support Public Health Decisions",
+          initials: "NT",
+          name: "Nirupama Tamvada (She/Her)",
+          role: "Statistician – BC Cancer Research Agency on the Regulatory Science Team",
           description:
-            "Key focus areas and research interests: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue nunc eu mi feugiat, sed egestas mi congue. Cras tristique, libero vitae pretium egestas.",
+            "Nirupama currently work as a statistician at the BC Cancer Research Agency on the Regulatory Science Team.",
         },
+        {
+          time: "2:00PM - 3:00PM",
+          topic: "Careers in Public Health - From Data to Decisions: How Models Can Support Public Health Decisions",
+          initials: "RF",
+          name: "Rebeca Falcao (She/Her)",
+          role: "Post Doctoral Fellow – BC Center for Disease Control",
+          description:
+            "Dr. Rebeca Cardim Falcao is a Postdoctoral Fellow at the BC Centre for Disease Control. She uses data science, mathematical modelling, and epidemiology to understand how infectious diseases spread and how to keep communities healthy. Her work focuses on improving disease surveillance and developing predictive tools that support epidemic preparedness, guide public health decisions, and strengthen modern health systems through evidence‑based approaches.",
+        }
       ],
     },
   };
